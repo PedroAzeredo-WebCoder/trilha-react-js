8c762985bab87deeb3ffac73902f05f2b2fe7bbb
Pedro Azeredo <pedro.azeredo93@gmail.com>
2021-10-18 09:21:56 -0300

    Aula 2 - Conhecendo mais sobre variaveis

--- declarandoVariaveis.js
+++ declarandoVariaveis.js
@@ -9,3 +9,11 @@ console.log(idade / 2);
 
 const idadeSomada = idade + 2;
 console.log(idadeSomada);
+
+console.log('Trabalhando com variaveis passo 2');
+
+idade = 29;
+Idade = 30; //Exemplo de como nao declarar variaveis
+
+console.log(idade);
+console.log(Idade);
